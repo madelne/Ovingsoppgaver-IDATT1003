bb01a7042f57d92c000e8102329aade4cd91bdde
Madeleine Negård <madelne@stud.ntnu.no>
2023-11-24 13:13:16 +0100

    Upload New File

---
+++ Øving 2/øving_2_oppgave_2.java
@@ -0,0 +1,15 @@
+package Øving_2;
+
+public class øving_2_oppgave_2 {
+    public static void main(String[]args){
+
+        double kiloprisA = 35.9 / 0.45;
+        double kiloprisB = 39.5 * 2;
+
+        if(kiloprisA < kiloprisB){
+            System.out.println("Kjottdeig av merke A er billigst");
+        }else{
+            System.out.println("Kjottdeig av merke B er billigst");
+        }
+    }
+}
